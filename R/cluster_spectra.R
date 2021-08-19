a6fe9ad82b1db1b85847aa61cefe0388b161ec40
Ethan Bass <ethanbass@gmail.com>
2021-08-19 15:42:08 -0400

    add 'save' option to cluster_spectra

--- R/cluster_spectra.R
+++ R/cluster_spectra.R
@@ -5,7 +5,7 @@ require(fastcluster)
 cluster_spectra <- function(pkTab, chrom_list, 
                             deepSplit = FALSE, peak_no = c(5,100),
                             alpha=0.95, nboot=1000, plot_dend=T, plot_spectra=T,
-                            verbose=T, parallel=T){
+                            verbose=T, save=T, parallel=T){
   if (verbose==T) print('...collecting representative spectra')
   rep <- sapply(1:ncol(pkTab), function(j){
     sp <- plot_spectrum(peak=j, peak_table=pkTab, chrom_list = dat.pr,
@@ -15,14 +15,14 @@ cluster_spectra <- function(pkTab, chrom_list,
   names(rep)<-paste0('V',1:ncol(rep))
   d<-1-abs(cor(rep,method="pearson"))
   
-  #cl<-fastcluster::hclust(as.dist(d), method = linkage)
-  #plot(cl)
   if (verbose==T) print('...clustering spectra')
   result <- pvclust(rep, method.dist="cor", method.hclust="average", nboot=nboot, parallel=parallel)
+  
   if (plot_dend==T){
   pvrect(result, alpha=alpha, max.only = F)
   }
-  #saveRDS(result, 'pvclust.RDS')
+  
+  if (save==T) saveRDS(result, 'pvclust.RDS')
   p <- pvpick(result, alpha=alpha, max.only=F)
   l <- sapply(p$clusters, length)
   sub <- p$clusters[which(l > mn & l < mx)]
@@ -30,6 +30,7 @@ cluster_spectra <- function(pkTab, chrom_list,
   sub <- lapply(1:length(sub), function(i) new("cluster", peaks=sub[[i]], pval=pval[i]))
   pval=format(round(result$edges[p$edges[which(l > mn & l < mx)],'au'],2), nsmall=2)
   names(sub)<-paste0('c',1:length(sub))
+  
   if (plot_spectra==T){
     if (verbose==T) print('...plotting clustered spectra')
     sapply(1:length(sub), function(i){ 
