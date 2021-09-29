34040271494293adac08b3432bc8532bdb28dcfc
Ethan Bass <ethanbass@gmail.com>
2021-09-29 11:35:29 -0400

    deprecate correctRT5, merge back into correctRT

--- R/correctRT5.R
+++ R/correctRT5.R
@@ -1,12 +1,14 @@
 correctRT5 <- function(CList, warpings, reference=1, ...){
-  
+  msg<-"The correctRT5 function is deprecated. You can now use the 'models' argument in correctRT to warp chromatograms according to an existing model"
+  .Deprecated(correctRT, package="chromatographR", msg,
+              old = as.character(sys.call(sys.parent()))[1L])
   allmats <- sapply(CList, identity, simplify = "array")
-  warpings2 <- lapply(warpings,FUN=function(x){
+  warp.coef <- lapply(warpings,FUN=function(x){
     x$warp.coef[1,]
   })
   
   ptwmods <- lapply((1:dim(allmats)[3]), function(ii){
-    ptw(t(allmats[,, reference]), t(allmats[, , ii]), init.coef=warpings2[[ii]],
+    ptw(t(allmats[,, reference]), t(allmats[, , ii]), init.coef=warp.coef[[ii]],
         try=TRUE, warp.type = "global")})
   
   result <- lapply(ptwmods, function(x) t(x$warped.sample))
