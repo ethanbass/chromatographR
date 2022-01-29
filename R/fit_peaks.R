9e55d979130202149e3066342140faeecb02ce51
Ethan Bass <ethanbass@gmail.com>
2022-01-29 15:38:12 -0500

    remove EMG function

--- R/fit_peaks.R
+++ R/fit_peaks.R
@@ -72,27 +72,6 @@ fit_peaks <- function (y, pos, sd.max = 50, fit = c("egh", "gaussian", "emg"),
         "height" = y[xloc], "area" = area, "r.squared" = r.squared)
     }
   }
-  else if (fit == "emg") {
-    tabnames <- c("rt","start","end", "sd", "tau", "FWHM", "height", "area", 
-                  "r.squared")
-    #tabnames <- c("rt", "sd", "lambda", "FWHM", "height", "area")
-    noPeaksMat <- matrix(rep(NA, length(tabnames)), nrow = 1, dimnames = list(NULL, 
-                                                               tabnames))
-    on.edge <- sapply(pos, function(x) y[x + 1] == 0 | y[x -
-                                                           1] == 0)
-    pos <- pos[!on.edge,]
-    if (length(pos) == 0) 
-      return(noPeaksMat)
-    fitpk <- function(xloc) {
-      xloc <- pos[1]
-      peak.loc <- seq.int(pos[2], pos[3])
-      m <- fit_EMG(peak.loc, y[peak.loc], start.center = xloc, 
-                   start.height = y[xloc])
-      r.squared <- try(summary(lm(m$y ~ y[peak.loc]))$r.squared, silent=T)
-      c(m$center, pos[2], pos[3], m$width, m$tau, 2.35 * m$width, y[xloc], 
-        y[xloc]/dnorm(m$center, m$center, m$width), r.squared)
-    }
-  }
   huhn <- data.frame(t(apply(pos, 1, fitpk)))
   colnames(huhn) <- tabnames
   huhn <- data.frame(sapply(huhn, as.numeric))
@@ -244,85 +223,7 @@ fit_egh <- function(x1, y1, start.center=NULL, start.width=NULL, start.tau=NULL,
   return(out)
 }
 
-####################################
-
-EMG <- function (x, mu = 0, sigma = 1, height = 1, alpha = 1){
-  f1 <- function (x, alpha, sigma, mu) 
-    exp(sigma^2/(2 * alpha^2) + (mu - x)/alpha)
-  f2 <- function (alpha, sigma) 
-    sqrt(2 * pi) * sigma/alpha
-  f3 <- function (x, alpha, sigma, mu) 
-  {
-    z <- (sigma/alpha + (mu - x)/sigma)/sqrt(2)
-    erfc(z)
-  }
-  erfc <- function (z) 
-    pnorm(z * sqrt(2), lower.tail = FALSE)
-  height * f1(x, alpha, sigma, mu) * f2(alpha, sigma) * f3(x, alpha, sigma, 
-                                                           mu)
-}
-
-fit_EMG <- function(x1, y1, start.center=NULL, start.width=NULL, start.alpha=NULL, start.height=NULL,
-                    start.floor=NULL, fit.floor=FALSE) {
-  # make some rough estimates from the values of Y
-  who.max <- which.max(y1)
-  if (is.null(start.center)){
-    start.center <- x1[who.max]
-  }
-  if (is.null(start.height)){
-    start.height <- y1[who.max]
-  }
-  if (is.null(start.width)){
-    start.width <- sum(y1 > (start.height/2)) / 2
-  }
-  if (is.null(start.alpha)){
-    start.alpha <- 1
-  }
-  # call the Nonlinear Least Squares, either fitting the floor too or not
-  controlList <- nls.control( maxiter=100, minFactor=1/512, warnOnly=TRUE)
-  if (!fit.floor){
-    starts <- list("mu"=start.center, "sigma"=start.width, "height"=start.height, "alpha"=start.alpha)
-    nlsAns <- try(nlsLM(y1 ~ EMG(x1, mu, sigma, height, alpha), start=starts, control=controlList,
-                      lower = c(0,0,0,0), upper=c(Inf,Inf,Inf,Inf), algorithm="port"), silent=T)
-  }else {
-    if (is.null( start.floor)) start.floor <- quantile( y, seq(0,1,0.1))[2]
-    starts <- list( "mu"=start.center, "sigma"=start.width, "height"=start.height, "alpha"=start.alpha, 
-                    "floor"=start.floor)
-    nlsAns <- try(nlsLM(y ~ EMG(x, mu, sigma, height, alpha), start=starts, control=controlList,
-               lower = c(0,0,0,0), upper=c(Inf,Inf,Inf,Inf), algorithm="port"), silent=T)
-  }
-  
-  # package up the results to pass back
-  if ( class( nlsAns) == "try-error") {
-    centerAns <- start.center
-    widthAns <- start.width
-    heightAns <- start.height
-    alphaAns <- start.alpha
-    floorAns <- if ( fit.floor) start.floor else 0
-    yAns <- EMG( x, centerAns, widthAns, heightAns, alphaAns, floorAns)
-    residualAns <- y - yAns
-  } else {
-    coefs <-coef(nlsAns)
-    centerAns <- coefs[1]
-    widthAns <- coefs[2]
-    heightAns <- coefs[3]
-    tauAns <- coefs[4]
-    floorAns <- if ( fit.floor) coefs[5] else 0
-    yAns <- fitted( nlsAns)
-    residualAns <- residuals( nlsAns)
-  }
-  
-  # always report the SD as a possitive value
-  widthAns <- abs( widthAns) #width = SD
-  
-  out <- list( "center"=centerAns, "width"=widthAns, "height"=heightAns, "tau"=tauAns, "y"=yAns,
-               "residual"=residualAns)
-  if ( fit.floor) {
-    out <- c( out, "floor"=floorAns)
-  }
-  
-  return(out)
-}
+#########################
 
 fitpeaks_at_max <- function (mat, pos, w=5, sd.max=50, fit=c("gaussian","egh")){
   #names(y) <- NULL
