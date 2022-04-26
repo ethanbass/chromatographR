05a5dd52f84f1ad2fc7755ceb2eaf784e6715362
Ethan Bass <ethanbass@gmail.com>
2022-04-26 10:26:09 -0400

    update combine_peaks function

--- R/combine_peaks.R
+++ R/combine_peaks.R
@@ -1,8 +1,9 @@
 #' Combine peaks in peak table
 #' 
 #' Utility function to combine duplicate peaks in peak table, i.e. peaks that
-#' were integrated at more than one wavelength or component. Specify number of
-#' digits to match retention time and minimum spectral correlation for a match.
+#' were integrated at more than one wavelength or component. Specify tolerance
+#' (\code{tol}) for retention time matching and minimum spectral correlation
+#' (\code{min.cor}) for a match.
 #' 
 #' @param peak_table Peak table from \code{\link{get_peaktable}}.
 #' @param tol Tolerance for matching retention times.
@@ -17,29 +18,32 @@
 #' combine_peaks(pk_tab, tol = .02, min.cor = .9)}
 #' @export combine_peaks
 combine_peaks <- function(peak_table, tol=.01, min.cor=0.9, choose='max'){
-  RTs <- as.numeric(peak_table$pk_meta['RT',])
-  mat <- outer(RTs, RTs, elementwise.all.equal, tolerance=0.01, scale=1)
-  # diag(mat) <- 0
-  d <- which(apply(mat,2, sum)>1)
+  if (!is.data.frame(peak_table$ref_spectra)){
+    stop("No reference spectra found. Use attach_ref_spectra function first.")
+  }
+  RTs <- as.numeric(peak_table$pk_meta['rt',])
+  compare_rts <- function(rt1, rt2, tol){
+    abs(rt1 - rt2) < tol
+  }
+  mat <- outer(RTs, RTs, compare_rts, tol=tol)
+  # find columns with a retention time match
+  d <- which(apply(mat,2, sum) > 1)
+  cors <- cor(peak_table$tab)
+  # iterate over columns with retention time match
+  # find columns to remove (sub)
   sub <- sapply(d, function(e){
-    i <- which(mat[,e]==1)
-    cors <- cor(peak_table$tab[,i])
-    j <- which(cors[,1] > min.cor)
-    if(length(j) > 1){
+    i <- which(mat[,e] == 1)
+    # compare spectral correlation among rt matches
+    j <- which(cors[i,e] > min.cor)
+    k <- i[j]
+    if (length(k) > 1){
       if (choose == 'max'){
-        sub <- names(sort(colSums(peak_table$tab[,i]),decreasing = TRUE)[-1])
+        sub <- names(sort(colSums(peak_table$tab[,k]), decreasing = TRUE)[-1])
       }
       which(colnames(peak_table$tab) %in% sub)
-      # peak_table$tab <- peak_table$tab[,-sub]
     }
   })
   peak_table$tab <- peak_table$tab[, -unlist(sub)]
   peak_table$pk_meta <- peak_table$pk_meta[, -unlist(sub)]
   return(peak_table)
 }
-
-# sub <- rbind(
-#   apply(peak_table$tab[,i], 2, max),
-#   peak_table$pk_meta["Component",i])
-# names(sort(colSums(peak_table$tab[,i]), decreasing = TRUE)[-1]),
-# peak_table$pk_meta["Component",i])
