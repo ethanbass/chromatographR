941c9391c086db4013e92a131e0a36081208e588
Ethan Bass <ethanbass@gmail.com>
2022-05-21 14:22:30 -0400

    update cluster_spectra function & expand docs
    allow cluster_spectra to make use of ref_spectra slot
    add switch so it can return the clusters and/or the actual pvclust object

--- R/cluster_spectra.R
+++ R/cluster_spectra.R
@@ -1,18 +1,30 @@
 setClass("cluster", representation(peaks = "character", pval = "numeric"))
 
-#' Function to cluster peaks by spectral similarity.
+#' Cluster peaks by spectral similarity.
 #' 
 #' Function to cluster peaks by spectral similarity. A representative spectrum
 #' is selected for each peak in the provided peak table and used to construct a
 #' distance matrix based on spectral similarity (pearson correlation) between
-#' peaks. Currently, representative spectrum is just selected from the
-#' chromatogram with the highest absorbance at lambda max. Hierarchical
-#' clustering with bootstrap resampling is performed on the resulting
-#' correlation matrix, as implemented in the
-#' \code{\link[pvclust:pvclust]{pvclust}} package. Bootstrap values can be used
+#' peaks. Hierarchical clustering with bootstrap resampling is performed on the resulting
+#' correlation matrix to classify peaks into by their spectral similarity.
+#'
+#' A representative spectrum is selected for each peak in the provided peak table
+#' and used to construct a distance matrix based on spectral similarity
+#' (pearson correlation) between peaks. It is suggested to attach representative
+#' spectra to the \code{peak_table} using \code{\link{attach_ref_spectra}}.
+#' Otherwise, representative spectra are obtained from the chromatogram with the
+#' highest absorbance at lambda max.
+#'
+#' Hierarchical clustering with bootstrap
+#' resampling is performed on the resulting correlation matrix, as implemented in
+#' \code{\link[pvclust:pvclust]{pvclust}}. Finally, bootstrap values can be used
 #' to select clusters that exceed a certain confidence threshold as defined by
-#' alpha.
-#' 
+#' \code{alpha}. Clusters can also be filtered by the minimum and maximum
+#' size of the cluster using the argument \code{peak_no}. If \code{max_only}
+#' is TRUE, only the largest cluster in a nested dendrogram of clusters meeting
+#' the confidence threshold will be returned.
+#'
+#' @name cluster_spectra
 #' @importFrom pvclust pvclust pvrect pvpick
 #' @importFrom stats cor
 #' @importFrom methods new
@@ -33,49 +45,77 @@ setClass("cluster", representation(peaks = "character", pval = "numeric"))
 #' @param parallel Logical. If TRUE, use parallel processing for
 #' \code{\link[pvclust:pvclust]{pvclust}}.
 #' @param max.only Logical. If TRUE, returns only highest level for nested
-#' dendrograms.
+#' dendrograms. 
+#' @param output What to return. Either \code{clusters} to return list of clusters,
+#' \code{pvclust} to return pvclust object, or \code{both} to return both items.
 #' @param ... Additional arguments to \code{\link[pvclust:pvclust]{pvclust}}.
-#' @return Returns S4 "cluster" object with the following components:
-#' \item{peaks}{a character vector containing the names of all peaks contained
-#' in the given cluster.} \item{pval}{a numeric vector of length 1 containing
-#' the bootstrap p-value (au) for the given cluster.}
+#' @return Returns clusters and/or \code{pvclust} object according to the value
+#' of the \code{output} argument.
+#' * If \code{output = clusters},  returns a list of S4 \code{cluster} objects.
+#' * If \code{output = pvclust}, returns a \code{\link[pvclust:pvclust]{pvclust}}
+#' object.
+#' * If \code{output = both}, returns a nested list containing \code{[[1]]} the
+#' \code{\link[pvclust:pvclust]{pvclust}} object, and \code{[[2]]} the list of
+#' S4 \code{cluster} objects.
+#'
+#' The \code{cluster} objects consist of the following components:
+#' * \code{peaks}: a character vector containing the names
+#' of all peaks contained in the given cluster.
+#' * \code{pval}: a numeric vector of length 1 containing
+#' the bootstrap p-value (au) for the given cluster.
 #' @author Ethan Bass
-#' @references R. Suzuki, H. Shimodaira: Pvclust: an R package for assessing
-#' the uncertainty in hierarchical clustering. Bioinformatics, 22-12:1540-1542
-#' (2006). \doi{10.1093/bioinformatics/btl117}.
+#' @references R. Suzuki & H. Shimodaira. 2006. Pvclust: an R package for assessing
+#' the uncertainty in hierarchical clustering. \emph{Bioinformatics},
+#' \bold{22(12)}:1540-1542. \doi{10.1093/bioinformatics/btl117}.
+#' @note
+#' * Users should be aware that the clustering algorithm will often return nested
+#' clusters. Thus, an individual peak could appear in more than one cluster.
+#' * It is highly suggested to use more than 100 bootstraps if you run the 
+#' clustering algorithm on real data even though we use \code{nboot = 100} in
+#' the example to reduce runtime. The authors of \code{pvclust} suggest \code{
+#' nboot = 10000}.
 #' @examples \donttest{
 #' data(pk_tab)
 #' data(Sa_warp)
 #' cl <- cluster_spectra(pk_tab, nboot=100, max.only = FALSE, save = FALSE, alpha = .97)
 #' }
 #' @export cluster_spectra
+#' @md
 
 cluster_spectra <- function(peak_table, chrom_list, peak_no = c(5,100),
                             alpha=0.95, nboot=1000, plot_dend=TRUE,
                             plot_spectra=TRUE, verbose=TRUE, save=TRUE,
                             parallel=TRUE, max.only=FALSE,
+                            output=c("clusters", "pvclust", "both"),
                             ...){
   if (missing(chrom_list)){
     chrom_list <- try(get(peak_table$args["chrom_list"]))
     if (inherits(chrom_list, "try-error")) stop("Chromatograms not found!")
   }
-  if (verbose) print('...collecting representative spectra')
-  rep <- sapply(colnames(peak_table[[1]]), function(j){
-    sp <- plot_spectrum(loc=j, peak_table=peak_table, chrom_list,
-                        scale_spectrum=TRUE, plot_trace=FALSE,
-                        export_spectrum=TRUE, plot_spectrum=FALSE, verbose=FALSE)
-  })
-  rep <- data.frame(do.call(cbind,rep))
-  names(rep) <- paste0('V',seq_len(ncol(rep)))
-  d<-1-abs(cor(rep,method="pearson"))
+  output <- match.arg(output, c("clusters","pvclust","both"))
+  if (is.data.frame(peak_table$ref_spectra) | is.matrix(peak_table$ref_spectra)){
+    rep <- peak_table$ref_spectra
+  } else{
+    if (verbose)
+      print('...collecting representative spectra')
+    rep <- sapply(colnames(peak_table[[1]]), function(j){
+      sp <- plot_spectrum(loc=j, peak_table=peak_table, chrom_list,
+                          scale_spectrum=TRUE, plot_trace=FALSE,
+                          export_spectrum=TRUE, plot_spectrum=FALSE, verbose=FALSE)
+    })
+    rep <- data.frame(do.call(cbind,rep))
+    names(rep) <- paste0('V',seq_len(ncol(rep)))
+  }
+  d <- 1 - abs(cor(rep, method="pearson"))
   
-  if (verbose) print('...clustering spectra')
+  if (verbose)
+    print('...clustering spectra')
   result <- pvclust(rep, method.dist="cor",
                              nboot=nboot, parallel=parallel, ...)
   
   if (plot_dend){
-  plot(result,labels = FALSE, cex.pv=0.5, print.pv='au',print.num = FALSE)
-  pvrect(result, alpha=alpha, max.only = max.only)
+    plot(result,labels = FALSE, cex.pv=0.5, print.pv='au',print.num = FALSE)
+    pvrect(result, alpha=alpha, max.only = max.only)
   }
   if (save) saveRDS(result, 'pvclust.RDS')
   p <- pvpick(result, alpha=alpha, max.only=max.only)
@@ -99,5 +139,5 @@ cluster_spectra <- function(peak_table, chrom_list, peak_no = c(5,100),
                           format(round(sub[[i]]@pval,2),nsmall=2))
                           )})
   }
-  return(sub)
+  switch(output, "clusters" = sub, "pvclust" = result, "both" = list(result,sub))
 }
