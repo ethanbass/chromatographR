89e19738bec1e67044dafb2b7ee9694b02291480
Ethan Bass <ethanbass@gmail.com>
2023-08-20 17:01:42 -0400

    Update chromatographR-package.R

--- R/chromatographR-package.R
+++ R/chromatographR-package.R
@@ -5,7 +5,7 @@
 #' chromatographR
 #' 
 #' \tabular{ll}{ Package: \tab chromatographR\cr Type: \tab Package\cr Version:
-#' \tab 0.4.5 \cr Date: \tab 2022-11-05\cr License: GPL (>= 2) }
+#' \tab 0.6.0 \cr Date: \tab 2023-08-20\cr License: GPL (>= 2) }
 #' 
 #' @name chromatographR-package
 #' @aliases chromatographR-package chromatographR
