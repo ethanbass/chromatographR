87c09e51ab32c5eb1c052c257d4e806656e91fb6
Ethan Bass <ethanbass@gmail.com>
2025-05-30 11:27:26 -0500

    feat: added heatmap pots to plot.ptw_list
    Also support for plotting traces with ggplot or plotly

--- R/correct_peaks.R
+++ R/correct_peaks.R
@@ -11,10 +11,11 @@
 #' @param peak_list A `peak_list` object created by \code{\link{get_peaks}},
 #' containing a nested list of peak tables where the first level is the sample,
 #' and the second level is the spectral wavelength. Every component is described
-#' by a matrix where every row is one peak, and the columns contain information on
-#' retention time, peak width (FWHM), peak width, height, and area.
-#' @param mod_list A list of ptw models.
-#' @param chrom_list List of chromatograms supplied to create ptw models.
+#' by a matrix where each row corresponds to a feature, and the columns contain 
+#' information on that feature (e.g., retention time, peak width (FWHM), height,
+#' area, etc.)
+#' @param mod_list A list of \code{ptw} models.
+#' @param chrom_list List of chromatograms from which the ptw models are derived.
 #' @param match_names Logical. Whether to actively match the names of the 
 #' \code{peak_list} to the list of models (\code{mod_list}). Defaults to 
 #' \code{TRUE}.
@@ -75,17 +76,25 @@ correct_peaks <- function(peak_list, mod_list, chrom_list, match_names = TRUE){
 #' 
 #' @importFrom graphics matplot
 #' @param x A \code{ptw_list} object created by \code{\link{correct_rt}}.
+#' @param what What type of plot to return. Either \code{traces} or 
+#' \code{heatmap}.
+#' @param engine What plotting engine to use. Either \code{base}, \code{ggplot}
+#' or \code{plotly}
 #' @param lambdas Which lambdas to plot.
-#' @param legend Logical. Whether to label the plots.
-#' @param ... Additional arguments to \code{\link[graphics]{matplot}}.
+#' @param show_legend Logical. Whether to include sample legend.
+#' @param ... Additional arguments (placeholder).
 #' @author Ethan Bass
+#' @examplesIf interactive()
+#' data(Sa_pr)
+#' warp <- correct_rt(chrom_list = Sa_pr, what = "models", lambdas = 210)
+#' plot(warp)
 #' @export
 
-plot.ptw_list <- function(x, lambdas, legend = TRUE, ...){
-  oldpar <- par(no.readonly = TRUE)
-  on.exit(par(oldpar))
-  par(mfrow=c(2, 1), mar=c(2, 3, 2, 3))
-  
+plot.ptw_list <- function(x, what = c("traces", "heatmap"),
+                          engine = c("base", "ggplot", "plotly"),
+                          lambdas, show_legend = TRUE, ...){
+  what <- match.arg(what, c("traces", "heatmap"))
+  engine <- match.arg(engine, c("base", "ggplot", "plotly"))
   all.lambdas <- as.numeric(rownames(x[[1]]$warped.sample))
   ts <- as.numeric(colnames(x[[1]]$sample))
   
@@ -95,30 +104,98 @@ plot.ptw_list <- function(x, lambdas, legend = TRUE, ...){
   if (any(!(lambdas %in% all.lambdas))){
     stop("Lambdas not found. Please check argument and try again")
   }
-  
+  raw_chroms <- lapply(x, function(samp) t(samp$sample))
+  warped_chroms <- lapply(x, function(samp){
+    xx <- t(samp$warped.sample)
+    rownames(xx) <- colnames(samp$sample)
+    xx
+  })
   lambda.idx <- which(lambdas %in% all.lambdas)
-  # plot warped samples
-  plot.new()
-  plot.window(xlim = c(head(ts,1), tail(ts,1)),
-              ylim = c(0, max(sapply(x, function(xx) xx$warped.sample), 
-                              na.rm = TRUE)*1.2))
-  for (i in seq_along(x)){
-    matplot(ts, t(x[[i]]$warped.sample[lambda.idx, , drop = FALSE]),
-            type = 'l', add = TRUE)
-  }
-  if (legend){
-    legend("topright", legend = "ptw", bty = "n")
-  }
-  # plot reference
-  plot.new()
-  plot.window(xlim = c(head(ts,1), tail(ts,1)),
-              ylim = c(0, max(x[[i]]$reference, na.rm = TRUE)*1.2))
-  for (i in seq_along(x)){
-    matplot(ts, t(x[[i]]$sample[lambda.idx, , drop = FALSE]),
-            type = 'l', add = TRUE)
-  }
-  if (legend){
-    legend("topright", legend = "queries", bty = "n")
+  if (what == "traces"){
+    if (engine == "base"){
+      oldpar <- par(no.readonly = TRUE)
+      on.exit(par(oldpar))
+      par(mfrow=c(2, 1), mar=c(2, 3, 2, 3))
+      plot_chroms(raw_chroms, lambdas = lambdas, title = "Raw data",
+                  show_legend = show_legend)
+      plot_chroms(warped_chroms, lambdas = lambdas, title = "PTW alignment", 
+                  show_legend = show_legend)
+    } else {
+      p1 <- plot_chroms(raw_chroms, lambdas = lambdas, title = "Raw data",
+                        engine = engine, show_legend = show_legend)
+      p2 <- plot_chroms(warped_chroms, lambdas = lambdas, title = "PTW alignment",
+                        engine = engine, show_legend = show_legend)
+      if (engine == "plotly"){
+        p2 <- p2 |> plotly::style(showlegend = FALSE) |> plotly::layout(title="")
+      }
+      combine_plots <- switch(engine,
+                              plotly = purrr::partial(plotly::subplot, nrows = 2),
+                              ggplot = purrr::partial(cowplot::plot_grid, 
+                                                       nrow = 2))
+      p_c <- combine_plots(p1, p2)
+      if (engine == "plotly"){
+        p_c <- p_c |> plotly::layout(
+          annotations = list(
+            list(x = 0.5, y = 1, text = "Raw data", showarrow = F, 
+                 xref = 'paper', yref = 'paper', font = list(size = 16)),
+            list(x = 0.5, y = 0.45, text = "PTW alignments", showarrow = F, 
+                 xref = 'paper', yref = 'paper', font = list(size = 16))
+          )
+        )
+      }
+      p_c
+    }
+  } else if (what == "heatmap"){
+    original <- sapply(x, function(x) x$sample[1,])
+    warped <- sapply(x, function(x) x$warped.sample[1,])
+    times <- colnames(x[[1]]$reference)
+    rownames(original) <- colnames(x[[1]]$reference)
+    rownames(warped) <- colnames(x[[1]]$reference)
+    if (engine == "base"){
+      plot_chroms_heatmap_base(original, show_legend = FALSE,
+                               title = "Raw data")
+      plot_chroms_heatmap_base(warped, show_legend = FALSE,
+                               title = "PTW alignment")
+    } else{
+        if (engine == "ggplot"){
+        p_o <- plot_chroms_heatmap_ggplot(original, title = "Raw data")
+        p_w <- plot_chroms_heatmap_ggplot(warped, title = "PTW alignment")
+        p <- cowplot::plot_grid(p_o + ggplot2::theme(legend.position = "none"),
+                                p_w + ggplot2::theme(legend.position = "none"), 
+                                nrow = 2)
+        if (show_legend){
+            legend <- suppressWarnings(cowplot::get_legend(p_o + 
+                        ggplot2::theme(legend.box.margin = ggplot2::margin(0, 6, 0, 0))))
+            p <- cowplot::plot_grid(p, legend, ncol = 2, rel_widths  = c(1, 0.2))
+        }
+      } else if (engine == "plotly"){
+        p_o <- plot_chroms_heatmap_plotly(original)
+        p_w <- plot_chroms_heatmap_plotly(warped, show_legend = FALSE)
+        p <- plotly::subplot(p_o, p_w, nrows = 2, margin = 0.1)
+        p <- p |> plotly::layout(
+          margin = list(t = 50),
+          annotations = list(
+            list(
+              x = 0.5, y = 1.01, 
+              text = "Raw data", 
+              showarrow = FALSE,
+              xref = "paper", yref = "paper",
+              xanchor = "center", yanchor = "bottom",
+              font = list(size = 14)
+            ),
+            list(
+              x = 0.5, y = 0.41, 
+              text = "PTW alignment", 
+              showarrow = FALSE,
+              xref = "paper", yref = "paper",
+              xanchor = "center", yanchor = "bottom",
+              font = list(size = 14)
+            )
+          )
+        )
+      }
+      p
+    }
   }
 }
 
